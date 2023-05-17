82cc31d487b75bb523c64eec567d82ab7c755ebc
Josué Rodriguez <13837690+Rapidisimo@users.noreply.github.com>
2023-05-17 12:18:15 -0400

    initial commit

---
+++ src/Card.jsx
@@ -0,0 +1,12 @@
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+
+function Card() {
+  return (
+    <main>
+      <h1>First React Project Deployment</h1>
+    </main>
+  )
+}
+
+export default Card
